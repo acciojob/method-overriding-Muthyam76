e3c35fd184b6a7f14d3ef991c12f6f5ba78c80d6
muthyam <dadigemuthyam@gmail.com>
2022-12-16 20:56:55 +0530

    k

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,8 +1,11 @@
 package com.driver;
 
 public class Main {
-    public static void main(String[] args){
-        System.out.println("hello world");
+
+
+
+        public static void main(String[] args){
+       // System.out.println("hello world");
         B b=new B();
         b.meth();
         b.meth();
