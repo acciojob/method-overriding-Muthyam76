5e0f4eb39dbc779f532227979bd9f7c68c404112
muthyam <dadigemuthyam@gmail.com>
2022-12-29 11:00:12 +0530

    ghgf

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,13 +1,19 @@
 package com.driver;
 
 public class Main {
-
-
-
-        public static void main(String[] args){
-       // System.out.println("hello world");
-        B b=new B();
-        b.meth();
-        b.meth();
+    public static class A{
+        public String meth(){
+            return  "Invoking method from class A";
+        }
+    }
+    public static class B{
+        public String meth(){
+            return "Method is overridden in Extendend class B";
+        }
+    }
+    public static void main(String args[]){
+        System.out.println("hello world");
+        B b1 =new B();
+        b1.meth();
     }
 }
